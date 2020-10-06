df577ff7b80d582eb15a93a784a7e8c2ed3de005
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-10-05 22:40:25 -0300

    Added files via upload

--- 03_Algorithms-on-Graphs/w4_paths_in_graphs2/1_minimum_flight_cost/dijkstra_v1.py
+++ 03_Algorithms-on-Graphs/w4_paths_in_graphs2/1_minimum_flight_cost/dijkstra_v1.py
@@ -1,12 +1,12 @@
 #Uses python3
+# Good job! (Max time used: 0.80/10.00, max memory used: 49074176/536870912.)
 
 import sys
 from collections import deque
 
-# min_heap implementation
 
 # edges[a]: [(end, weight), ...]
-# H = [[node1, dist1], [node2, dist2], ...]
+# H = [[node1, distance1], [node2, distance2],...]
 class MinimumCost:
     def __init__(self, n, edges, start, end):
         self.edges = edges
@@ -18,7 +18,7 @@ class MinimumCost:
         self.start = start
         self.end = end
 
-    def siftDown(self, i):
+    def SiftDown(self, i):
         min_index = i
         left_child = 2 * i + 1
         right_child = 2 * i + 2
@@ -28,59 +28,55 @@ class MinimumCost:
             min_index = right_child
         if min_index != i:
             self.H[i], self.H[min_index] = self.H[min_index], self.H[i]
-            self.siftDown(min_index)
+            self.SiftDown(min_index)
 
-    def siftUp(self, i):
+    def SiftUp(self, i):
         while i > 0 and self.H[i][1] < self.H[(i - 1) // 2][1]:
             self.H[i], self.H[(i - 1) // 2] = self.H[(i - 1) // 2], self.H[i]
             i = (i - 1) // 2
 
-    def _siftUp(self, i):
+    def _SiftUp(self, i):
         while i > 0:
             parent = (i - 1) // 2
             if self.H[i][1] < self.H[parent][1]:
                 self.H[i], self.H[parent] = self.H[parent], self.H[i]
                 i = parent
 
-    def extractMin(self):
+    def ExtractMin(self):
         result = self.H[0]
         self.H[0] = self.H[len(self.H) - 1]
         self.H.pop()
-        self.siftDown(0)
+        self.SiftDown(0)
         return result
 
-    def insert(self, node, p):
+    def Insert(self, node, p):
         self.H.append([node, p])
-        self.siftUp(len(self.H) - 1)
+        self.SiftUp(len(self.H) - 1)
 
-    def changePriority(self, i, new):
+    def ChangePriority(self, i, new):
         self.H[i][1] = new
-        self.siftUp(i)
-
+        self.SiftUp(i)
 
     # edges[a]: [(end, weight), ...]
-    # H = [[node1, dist1], [node2, dist2], ...]
-    def dijkstra(self):
+    # H = [[node1, distance1], [node2, distance2],...]
+    def Dijkstra(self):
         self.dist[self.start] = 0
-        self.changePriority(self.start - 1, 0)
+        self.ChangePriority(self.start - 1, 0)
         while self.H:
-            u, dist_u = self.extractMin()
+            u, dist_u = self.ExtractMin()
             if not self.processed[u]:
                 self.processed[u] = True
                 for v, weight in self.edges[u]:
                     if self.dist[v] > self.dist[u] + weight:
                         self.dist[v] = self.dist[u] + weight
-                        self.insert(v, self.dist[u])
+                        self.Insert(v, self.dist[v])
         if self.dist[self.end] != float('inf'):
             return self.dist[self.end]
         else:
             return -1
 
 
-
-
 if __name__ == '__main__':
-    # TEST 2
     n_vertices, n_edges = map(int, input().split())
     edges = [[] for _ in range(n_vertices + 1)]
     for i in range(n_edges):
@@ -88,7 +84,7 @@ if __name__ == '__main__':
         edges[a].append((b, w))
     s, e = map(int, input().split())
     flights = MinimumCost(n_vertices, edges, s, e)
-    min_cost = flights.dijkstra()
+    min_cost = flights.Dijkstra()
     print(min_cost)
 
     # TEST 1
