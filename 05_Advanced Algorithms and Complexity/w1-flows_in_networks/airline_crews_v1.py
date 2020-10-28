91510a5f08edcedaca939338f8cd399d09f24c58
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-10-28 19:35:18 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w1-flows_in_networks/airline_crews_v1.py
+++ 05_Advanced Algorithms and Complexity/w1-flows_in_networks/airline_crews_v1.py
@@ -1,5 +1,18 @@
 # python3
 
+from collections import deque
+
+def make_network(n, m, bip):
+    pass
+
+def has_path(Gf, path):
+    pass
+
+def maxflow(Gf, n):
+    pass
+
+
+
 if __name__ == '__main__':
     n_flights, n_crew = map(int, input().split())
     bipartite = [list(map(int, input().split())) for i in range(n_flights)]
