b099f5d6e8f64f4f1e59b5214e171850f9423093
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-10-29 10:54:39 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w1-flows_in_networks/airline_crews_v1.py
+++ 05_Advanced Algorithms and Complexity/w1-flows_in_networks/airline_crews_v1.py
@@ -3,13 +3,59 @@
 from collections import deque
 
 def make_network(n, m, bip):
-    pass
+    '''
+    n: number of flights;
+    m: number of crews;
+    '''
+    graph = [[0] * (n+ m + 2) for _ in range(n + m + 2)]
+    for i in range(1, n + 1):
+        graph[0][i] = 1
+        for j in range(m):
+            graph[i][n + 1 + j] = bip[i - 1][j]
+    for k in range(n + 1, n + m + 1):
+        graph[k][-1] = 1
+    return graph
+
 
 def has_path(Gf, path):
-    pass
+    V = len(Gf)
+    visited = [False] * V
+    visited[0] = True
+    queue = deque([0])
+    while queue:
+        u = queue.popleft()
+        if u == V - 1:
+            return True
+        for v in range(V):
+            if not visited[v] and Gf[u][v] > 0:
+                queue.append(v)
+                visited[v] = True
+                path[v] = u
+    return visited[V - 1]
 
 def maxflow(Gf, n):
-    pass
+    V = len(Gf)
+    path = list(range(V))
+    while has_path(Gf, path):
+        min_flow = float('inf')
+        v = V - 1
+        while v != 0:
+            u = path[v]
+            min_flow = min(min_flow, Gf[u][v])
+            v = u
+        v = V - 1
+        while v != 0:
+            u = path[v]
+            Gf[u][v] -= min_flow
+            Gf[v][u] += min_flow
+            v = u
+    matches = [-1] * n
+    for i in range(V):
+        if Gf[V-1][i] == 1:
+            person = i - n
+            flight = Gf[i].index(1)
+            matches[flight - 1] = person
+    return matches
 
 
 
