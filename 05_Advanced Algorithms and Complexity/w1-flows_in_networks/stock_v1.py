f295afc818753e9323afd04e6215a40beb3f39d1
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-10-29 13:01:36 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w1-flows_in_networks/stock_v1.py
+++ 05_Advanced Algorithms and Complexity/w1-flows_in_networks/stock_v1.py
@@ -1,1 +1,24 @@
 # python3
+
+from colletions import deque
+
+def DAG(n, stock):
+    pass
+
+def make_network(n, adj):
+    pass
+
+def has_path(Gf, path):
+    pass
+
+def maxflow(Gf):
+    pass
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    stock_data = [list(map(int, input().split())) for _ in range(n)]
+    adj_matrix = DAG(n, stock_data)
+    graph = make_network(n, adj_matrix)
+    max_flow = maxflow(graph)
+    min_charts = n - max_flow
+    print(min_charts)
