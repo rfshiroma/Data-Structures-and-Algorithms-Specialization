7dab58a554853e340a599331b55bb5045cb0f9d6
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-10-31 20:28:42 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w2-Linear_Programming/energy-values_v1.py
+++ 05_Advanced Algorithms and Complexity/w2-Linear_Programming/energy-values_v1.py
@@ -20,3 +20,28 @@ class Position:
         b = []
         for _ in range(size):
             line = list(map(float, input().split()))
+            a.append(line[:size])
+            b.append(line[size])
+        return Equation(a, b)
+
+def select_pivot_element(pivot, a, used_rows):
+    pass
+
+# swap row to top of non-pivot rows
+def swap_lines(a, b, used_rows, pivot):
+    pass
+
+def process_pivot_element(a, b, pivot, used_rows):
+    pass
+
+def solve_equation(equation):
+    pass
+
+def print_column(column):
+    pass
+
+if __name__ == '__main__':
+    matrix = read_data()
+    solution = solve_equation(matrix)
+    print_column(solution)
+    exit(0)
