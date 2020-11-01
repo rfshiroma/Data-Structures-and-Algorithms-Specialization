46b0257db8d47f308f076d0aa44f67e928fd2ec5
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-10-31 21:47:25 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w2-Linear_Programming/energy-values_v1.py
+++ 05_Advanced Algorithms and Complexity/w2-Linear_Programming/energy-values_v1.py
@@ -14,31 +14,62 @@ class Position:
         self.row = row
         self.col = col
 
-    def read_data():
-        size = int(input())
-        a = []
-        b = []
-        for _ in range(size):
-            line = list(map(float, input().split()))
-            a.append(line[:size])
-            b.append(line[size])
-        return Equation(a, b)
+def read_data():
+    size = int(input())
+    a = []
+    b = []
+    for _ in range(size):
+        line = list(map(float, input().split()))
+        a.append(line[:size])
+        b.append(line[size])
+    return Equation(a, b)
 
 def select_pivot_element(pivot, a, used_rows):
-    pass
+    while used_rows[pivot.row] or a[pivot.row][pivot.col] == 0:
+        pivot.row += 1
+    return pivot
 
 # swap row to top of non-pivot rows
 def swap_lines(a, b, used_rows, pivot):
-    pass
+    a[pivot.col], a[pivot.row] = a[pivot.row], a[pivot.col]
+    b[pivot.col], b[pivot.row] = b[pivot.row], b[pivot.col]
+    used_rows[pivot.col], used_rows[pivot.row] = used_rows[pivot.row], used_rows[pivot.col]
+    pivot.row = pivot.col
+
 
 def process_pivot_element(a, b, pivot, used_rows):
-    pass
+    scale = a[pivot.row][pivot.col]
+    if scale != 1:
+        for i in range(len(a)):
+            a[pivot.row][i] /= scale
+        b[pivot.row] /= scale
+    for i in range(len(a)):
+        if i != pivot.row:
+            multiple = a[i][pivot.col]
+            for j in range(len(a)):
+                a[i][j] -= a[pivot.row][j] * multiple
+            b[i] -= b[pivot.row] * multiple
+    used_rows[pivot.row] = True
 
 def solve_equation(equation):
-    pass
+    a = equation.a
+    b = equation.b
+    size = len(a)
+    used_rows = [False] * size
+    for i in range(size):
+        pivot = Position(0, i)
+        pivot = select_pivot_element(pivot, a, used_rows)
+        # print(pivot.row, pivot.col)
+        swap_lines(a, b, used_rows, pivot)
+        process_pivot_element(a, b, pivot, used_rows)
+        # print(a, b)
+    return b
+
 
 def print_column(column):
-    pass
+    for e in column:
+        print("{0:.6f}".format(e), end=' ')
+
 
 if __name__ == '__main__':
     matrix = read_data()
