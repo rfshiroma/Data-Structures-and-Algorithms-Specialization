c0f6b6b118572ddaff40a95ded0a5dc786e6d271
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-11-03 23:07:09 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w3-NP-complete_Problems/2-cleaning_apartment_v1.py
+++ 05_Advanced Algorithms and Complexity/w3-NP-complete_Problems/2-cleaning_apartment_v1.py
@@ -1,14 +1,23 @@
 # python3
-n, m = map(int, input().split())
-edges = [ list(map(int, input().split())) for i in range(m) ]
+
+import itertools
+
+
+n, m = list(map(int, stdin.readline().split()))
+edges = [list(map(int, input().split())) for i in range(m)]
+
+A = []
+for i in range(n):
+  A += [list(map(int, stdin.readline().split()))]
+b = list(map(int, stdin.readline().split()))
 
 # This solution prints a simple satisfiable formula
 # and passes about half of the tests.
 # Change this function to solve the problem.
-def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
-
-printEquisatisfiableSatFormula()
+# def printEquisatisfiableSatFormula():
+#     print("3 2")
+#     print("1 2 0")
+#     print("-1 -2 0")
+#     print("1 -2 0")
+#
+# printEquisatisfiableSatFormula()
