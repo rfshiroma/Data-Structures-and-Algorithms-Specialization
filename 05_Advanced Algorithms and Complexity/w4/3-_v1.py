dfb1810f301cae2872c3ff53d0144a764010d74b
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-11-07 21:07:02 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w4/3-_v1.py
+++ 05_Advanced Algorithms and Complexity/w4/3-_v1.py
@@ -1,1 +1,23 @@
 # python3
+
+from itertools import permutations
+from itertools import combinations
+from collections import deque
+
+INF = 10**9
+
+
+def read_data():
+    pass
+
+
+def print_answer(path_weight, path):
+    pass
+
+
+def optimal_path(graph):
+    pass
+
+
+if __name__ == '__main__':
+    print_answer(*optimal_path(read_data()))
