77eaa305e4db698ea141ed80c63f5d003154ee53
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-11-07 21:12:54 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w4/4-_v1.py
+++ 05_Advanced Algorithms and Complexity/w4/4-_v1.py
@@ -1,1 +1,45 @@
 # python3
+
+import sys
+import threading
+
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**25)  # new thread will get stack of such size
+
+
+class Node:
+    def __init__(self, i):
+        self.r = 3 * i - 2
+        self.g = 3 * i - 1
+        self.b = 3 * i
+
+
+def reduce_to_two_SAT(color, edges):
+    pass
+
+
+def two_SAT_to_graph(n, C):
+    pass
+
+
+def post_order(i, graph, visited, post):
+    pass
+
+
+def DFS(n, graph):
+    pass
+
+
+def explore(i, graph, visited, SCC, SCC_number, u):
+    pass
+
+
+def find_SCCs(n, rev_graph, graph):
+    pass
+
+
+def solve_two_SAT(n, rev_graph, graph):
+    pass
+
+# This is to avoid stack overflow issues
+threading.Thread(target=main).start()
