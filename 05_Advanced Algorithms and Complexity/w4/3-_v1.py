da4f6c89febbe21ffb59a159a5a052e1575e4ea4
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-11-13 10:52:54 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w4/3-_v1.py
+++ 05_Advanced Algorithms and Complexity/w4/3-_v1.py
@@ -26,7 +26,26 @@ def print_answer(path_weight, path):
 
 
 def optimal_path(graph):
-    pass
+    n = len(graph)
+    C = [[INF for _ in range(n)] for __ in range(1 << n)]  # shift to left by n bits
+    backtrack = [[(-1, -1) for _ in range(n)] for __ in range(1 << n)] # shift to left by n bits
+    C[1][0] = 0
+    for size in range(1, n):
+        for S in combinations(range(1, n), size):
+            S = (0, ) + S
+            k = sum([1 << i for i in S])
+            for i in S:
+                if i != 0:
+                    for j in S:
+                        if j != i:
+                            m = k ^ (1 << i)
+                            curr = C[m][j] + graph[j][i]
+                            if curr < C[k][i]:
+                                C[k][i] = curr
+                                backtrack[k][i] = (m, j)
+    best_ans, index2 = min([(C[-1][i] + graph[i][0], i) for i in range(n)])
+
+    
 
 
 if __name__ == '__main__':
