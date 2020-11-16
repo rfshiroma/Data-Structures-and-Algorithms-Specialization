8ad2b5f0882ecfe31f2a5373141da99137dc77e4
ryan_shiroma@hotmail.com <ryan_shiroma@hotmail.com>
2020-11-16 20:52:35 -0300

    Added files via upload

--- 05_Advanced Algorithms and Complexity/w4/4-_v1.py
+++ 05_Advanced Algorithms and Complexity/w4/4-_v1.py
@@ -15,7 +15,27 @@ class Node:
 
 
 def reduce_to_two_SAT(color, edges):
-    pass
+    n = len(colors)
+    C = []
+    for i in range(1, n + 1):
+        node = Node(i)
+        if colors[i - 1] == 'R':
+            # C.append((-node.r))
+            C.append((node.g, node.b))
+            C.append((-node.g, -node.b))
+        elif colors[i - 1] == 'G':
+            # C.append((-node.g))
+            C.append((node.r, node.b))
+            C.append((-node.r, -node.b))
+        elif colors[i - 1] == 'B':
+            C.append((node.r, node.g))
+            C.append((-node.r, -node.g))
+    for a, b in edges:
+        u, v = Node(a), Node(b)
+        C.append((-u.r, -v.r))
+        C.append((-u.g, -v.g))
+        C.append((-u.b, -v.b))
+    return C
 
 
 def two_SAT_to_graph(n, C):
@@ -40,6 +60,8 @@ def find_SCCs(n, rev_graph, graph):
 
 def solve_two_SAT(n, rev_graph, graph):
     pass
+
+
 
 # This is to avoid stack overflow issues
 threading.Thread(target=main).start()
